9c13db03a9a9bea2bcb956d9ca2eee886a31c423
Ifekingsley <97189174+Ifekingsley@users.noreply.github.com>
2024-02-27 21:48:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -25,8 +25,8 @@ global.Owner = ["263715289858", "265889976968"];         //If you want singal nu
 global.OwnerNumber = ["263715289858", "265889976968"];   //If you want singal number so global.Owner = ['916297175943'] Change into your number.
 global.ownertag = ["265889976968"];
 global.OwnerName = "Kingsley";
-global.BotName = "A17";
-global.packname = "A17 Bot";                             //Do not change.
+global.BotName = "A17 God";
+global.packname = "A17 Got";                             //Do not change.
 global.author = "By: Kingsley";                               //Do not change.
 global.BotSourceCode = "https://github.com/Ifekingsley/A17"; //Do not change.
 global.SupportGroupLink = "https://chat.whatsapp.com/GhRGdwfaMVDCoeAdzILfl";
@@ -41,11 +41,11 @@ global.openAiAPI = "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";
 
 
 //
-global.location = "Tata City, India";
+global.location = "south africa, MW";
 global.reactmoji = "❤️";
 global.themeemoji = "💖";
 global.vidmenu = { url: 'https://media.tenor.com/Jdu0Ov8X2sIAAAAC/A17-Bot.mp4' };
-global.websitex = "https://github.com/Kai0071";
+global.websitex = "https://github.com/ifekingsley";
 global.lolhuman = "KaysaS";
 
 
@@ -79,19 +79,19 @@ global.ntilink = []
 
 //
 global.mess = {
-  jobdone: 'Here you go...',
-  useradmin: 'Sorry, only *Group Admins* can use this command *Baka*!',
-  botadmin: 'Sorry, i cant execute this command without being an *Admin* of this group.',
-  botowner: 'Only my *Owner* can use this command, Baka!',
-  grouponly: 'This command is only made for *Groups*, Baka!',
-  privateonly: 'This command is only made for *Private Chat*, Baka!',
-  botonly: 'Only the *Bot itself* can use this command!',
-  waiting: 'Just Wait...',
-  nolink: 'Please provide me *link*, Baka!',
-  error: 'An error occurd!',
-  banned: 'You are *Banned* fron using commands!',
-  bangc: 'This Group is *Banned* from using Commands!',
-  nonsfw: 'Dont be a pervert Baka! This is not a NSFW enabled group!'
+  jobdone: 'Here you go low class mortal...',
+  useradmin: 'Sorry, only *Group Gods* can use this command *Mortal*!',
+  botadmin: 'Sorry, i cant execute this command without being a *God* of this mortal group.',
+  botowner: 'Only my *God Owner* can use this command, Mortal!',
+  grouponly: 'This command is only made for *Mortal Groups*, Mortal!',
+  privateonly: 'This command is only made for *Gods Private Chat*, Mortal!',
+  botonly: 'Only the *God bot itself* can use this command!',
+  waiting: 'Just Wait Mortal...',
+  nolink: 'Please provide me *link*, Mortal!',
+  error: 'An error occurd on Gods end!',
+  banned: 'You are *Banned* fron using commandments!',
+  bangc: 'This Group is *Banned* from using Commandments!',
+  nonsfw: 'Dont be a pervert Mortal! This is not a NSFW enabled group!'
 
 }
 
